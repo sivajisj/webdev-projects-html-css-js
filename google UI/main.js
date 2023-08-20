57de2c8098fa9819b9aeb800ec770e5f3d81d0bc
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2023-08-20 19:41:12 +0530

    files are added

---
+++ google UI/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".input-field").focusout(function(){
+        var inputValue = $(this).val();
+        if(inputValue ===""){
+            $(this).removeClass("has-value");
+        }else{
+            $(this).addClass("has-value");
+        }
+    })
+})
